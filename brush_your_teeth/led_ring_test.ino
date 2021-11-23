056d2f59e6f89d12dda5b97a4af31fee5c785374
Ben <benski3030@gmail.com>
2021-11-23 16:32:51 -0600

    brush your teeth!

---
+++ brush_your_teeth/led_ring_test.ino
@@ -0,0 +1,220 @@
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+// Pins
+#define APDS9960_INT    2  // Needs to be an interrupt pin
+#define LED_PIN         13 // LED for showing interrupt
+#define PIN 6
+
+// Constants
+#define PROX_INT_HIGH   50 // Proximity level for interrupt
+#define PROX_INT_LOW    0  // No far interrupt
+
+// Global variables
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+uint8_t proximity_data = 0;
+int isr_flag = 0;
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+void setup() {
+    // Set LED as output
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(APDS9960_INT, INPUT);
+  
+  // Initialize Serial port
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println(F("---------------------------------------"));
+  Serial.println(F("SparkFun APDS-9960 - ProximityInterrupt"));
+  Serial.println(F("---------------------------------------"));
+  
+  // Initialize interrupt service routine
+  attachInterrupt(0, interruptRoutine, FALLING);
+  
+  // Initialize APDS-9960 (configure I2C and initial values)
+  if ( apds.init() ) {
+    Serial.println(F("APDS-9960 initialization complete"));
+  } else {
+    Serial.println(F("Something went wrong during APDS-9960 init!"));
+  }
+  
+  // Adjust the Proximity sensor gain
+  if ( !apds.setProximityGain(PGAIN_2X) ) {
+    Serial.println(F("Something went wrong trying to set PGAIN"));
+  }
+  
+  // Set proximity interrupt thresholds
+  if ( !apds.setProximityIntLowThreshold(PROX_INT_LOW) ) {
+    Serial.println(F("Error writing low threshold"));
+  }
+  if ( !apds.setProximityIntHighThreshold(PROX_INT_HIGH) ) {
+    Serial.println(F("Error writing high threshold"));
+  }
+  
+  // Start running the APDS-9960 proximity sensor (interrupts)
+  if ( apds.enableProximitySensor(true) ) {
+    Serial.println(F("Proximity sensor is now running"));
+  } else {
+    Serial.println(F("Something went wrong during sensor init!"));
+  }
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  // If interrupt occurs, print out the proximity level
+  if ( isr_flag == 1 ) {
+  
+    // Read proximity level and print it out
+    if ( !apds.readProximity(proximity_data) ) {
+      Serial.println("Error reading proximity value");
+      //colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
+    } else {
+      //theaterChaseRainbow(50);
+      Serial.print("Proximity detected! Level: ");
+      Serial.println(proximity_data);
+    }
+    
+    // Turn on LED for a half a second
+    theaterChaseRainbow(50);
+    //colorWipe(strip.Color(0, 0, 0), 50);
+    //digitalWrite(LED_PIN, HIGH);
+    delay(5500);
+    //digitalWrite(LED_PIN, LOW);
+    colorWipe(strip.Color(0, 0, 0), 50);
+    
+    // Reset flag and clear APDS-9960 interrupt (IMPORTANT!)
+    isr_flag = 0;
+    if ( !apds.clearProximityInt() ) {
+      Serial.println("Error clearing interrupt");
+    }
+    
+  }
+  // Some example procedures showing how to display to the pixels:
+  //colorWipe(strip.Color(255, 0, 0), 50); // Red
+  //colorWipe(strip.Color(0, 255, 0), 50); // Green
+  //colorWipe(strip.Color(0, 0, 255), 50); // Blue
+//colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
+  // Send a theater pixel chase in...
+  //theaterChase(strip.Color(127, 127, 127), 50); // White
+  //theaterChase(strip.Color(127, 0, 0), 50); // Red
+  //theaterChase(strip.Color(0, 0, 127), 50); // Blue
+
+  //rainbow(20);
+  //rainbowCycle(20);
+  //theaterChaseRainbow(50);
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void interruptRoutine() {
+  isr_flag = 1;
+}
